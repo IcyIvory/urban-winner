b1da70b7ad06ee6f95c8cc21aa0b3259457b73cd
IcyIvory <106037974+IcyIvory@users.noreply.github.com>
2022-09-12 15:02:00 -0400

    Added nested_dicts.py
    Working on nested dictionaries.

---
+++ nested_dicts.py
@@ -0,0 +1,27 @@
+users = {
+    'vgarden': {
+        'first_name': 'violet',
+        'last_name': 'garden',
+        'age': '20',
+        'city': 'halifax'
+    },
+    'dotoole': {
+        'first_name': 'doc',
+        'last_name': "o'toole",
+        'age': '46',
+        'city': 'haliburton'
+    },
+    'whouston': {
+        'first_name': 'windy',
+        'last_name': 'houston',
+        'age': '32',
+        'city': 'houston'
+    }
+}
+
+for username, info in users.items():
+    print(f'\nUsername: {username}')
+    full_name = f"{info['first_name'].title()} {info['last_name'].title()}"
+    location = info['city'].title()
+
+    print(f"{full_name}, age {info['age']}, of {location}")
